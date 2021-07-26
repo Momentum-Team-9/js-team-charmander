08b361f623a19141559e75ea974e142d9866c552
Em Sparr <amberem@gmail.com>
2021-07-26 13:56:56 -0400

    Comments outlining js formatting plans

--- main.js
+++ main.js
@@ -1,1 +1,15 @@
 const url = "https://itunes.apple.com/search?"
+
+
+// Function to create each song card, adding it to the songs section
+// // Make sure to allow space for album image and song details.
+
+// Event Listener to pull search results
+
+// Function to format the search input correctly for the iTunes API
+
+// CRUD function to GET songs by artist or band
+
+// Event Listener to play song preview
+
+// Event Listener to stop song preview
